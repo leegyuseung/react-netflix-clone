7a865265faaae060fba3d4b016dc7e4a2f21ede5
leegyuseung <lgs4002@naver.com>
2022-08-16 23:05:19 +0900

    React Router Dom 적용

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -54,9 +54,33 @@ const Banner = () => {
               className="banner__button play"
               onClick={() => setIsClicked(true)}
             >
-              Play
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-play-circle"
+                viewBox="0 0 16 16"
+              >
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z" />
+              </svg>
+              <span style={{ paddingLeft: 10 }}>Play</span>
             </button>
-            <button className="banner__button info">More Information</button>
+            <button className="banner__button info">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-info-circle"
+                viewBox="0 0 16 16"
+              >
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
+              </svg>
+              <span style={{ paddingLeft: 10 }}>More Information</span>
+            </button>
           </div>
           <h1 className="banner__description">
             {truncate(movie?.overview, 100)}
