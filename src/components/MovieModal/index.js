1b4c556f43f40399a3cd0c6f07194163b16a2a13
leegyuseung <lgs4002@naver.com>
2022-08-17 23:49:34 +0900

    modal 바깥 클릭시 종료하는 customhooks 제작

--- src/components/MovieModal/index.js
+++ src/components/MovieModal/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useRef } from "react";
+import useOnClickOutside from "../../hooks/useOnClickOutside";
 import "./MovieModal.css";
 
 const MovieModal = ({
@@ -11,10 +12,16 @@ const MovieModal = ({
   vote_average,
   setModalOpen,
 }) => {
+  // Ref를 이용해서 모달밖을 클릭시 모달 종료하게 하기.
+  const ref = useRef();
+  useOnClickOutside(ref, () => {
+    setModalOpen(false);
+  });
+
   return (
     <div className="presentation" role="presentation">
       <div className="wrapper-modal">
-        <div className="modal">
+        <div className="modal" ref={ref}>
           <span onClick={() => setModalOpen(false)} className="modal-close">
             X
           </span>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -34,7 +34,7 @@ const Nav = () => {
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
         className="nav__logo"
         onClick={() => {
-          window.location.reload();
+          navigate("/");
         }}
       />
 
